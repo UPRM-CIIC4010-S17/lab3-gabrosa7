c164fdd235ea80ba695cb8dc31068feaea7c42f9
Gabriel <gabriel.rosa4@upr.edu>
2017-12-06 00:45:25 -0400

    Set the frame in display center & change frame name and size

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,31 @@
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		//set frame height and width
+		int frameWidth = 200;
+		int frameHeight = 200;
+		
+		//Set frame location x and y value in order to set it in screen center
+		int frameX = (int) (userWindowWidth() / 2) - (frameWidth / 2);
+		int frameY = (int) (userWindowHeight() / 2) - (frameHeight / 2);
+		
+		JFrame myFrame = new JFrame("Gabriel J. Rosa Rios");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(frameX, frameY);
+		myFrame.setSize(frameWidth,frameHeight);
 		myFrame.setVisible(true);
+	}
+	
+	public static double userWindowWidth() {
+		double screenW = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+		return screenW;
+	}
+	
+	public static double userWindowHeight() {
+		double screenH = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+		return screenH;
 	}
 }
